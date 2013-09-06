53bac168ffb664e3faca1346b3027b91b55ce6e8
pedro <plazanet.pierre@gmail.com>
2013-09-06 17:45:12 +0200

    Add step to mailer context

--- src/Knp/FriendlyContexts/Context/SwiftMailerContext.php
+++ src/Knp/FriendlyContexts/Context/SwiftMailerContext.php
@@ -26,8 +26,8 @@ class SwiftMailerContext extends RawMinkContext implements KernelAwareInterface
     }
 
     /**
-     * @Then /^(\d+) mail should be sent$/
-     * @Then /^(\d+) mails should be sent$/
+     * @Then /^(\d+) mail should have been sent$/
+     * @Then /^(\d+) mails should have been sent$/
      */
     public function mailShouldBeSent($expected)
     {
@@ -42,12 +42,64 @@ class SwiftMailerContext extends RawMinkContext implements KernelAwareInterface
             )
         );
 
-
         $this->assertEquals(
             (int) $expected,
             (int) $real,
             sprintf('fail to assert %s mail(s) sent, %s sent in reality.', $expected, $real)
         );
+    }
+
+    /**
+     * @Given /^an email with subject "([^"]*)" should have been sent to "([^"]*)"$/
+     */
+    public function anEmailWithSubjectShouldHaveBeenSentTo($subject, $email)
+    {
+        $messages = array_filter(
+            $this->messages,
+            function ($e) use ($subject) {
+                return false !== strpos(strtoupper($e->getSubject()), strtoupper($subject));
+            }
+        );
+
+        if (0 === count($messages)) {
+            throw new \Exception(sprintf(
+                'Can\'t find email with subject containing "%s", "%s" found',
+                $subject,
+                implode(
+                    '" or "',
+                    array_map(
+                        function ($e) {
+                            return $e->getSubject();
+                        },
+                        $this->messages
+                    )
+                )
+            ));
+        }
+
+        $emails = [];
+
+        foreach ($messages as $message) {
+            $emails = array_merge($emails, $message->getTo());
+            $emails = array_merge($emails, $message->getCc());
+            $emails = array_merge($emails, $message->getBcc());
+        }
+
+        $emails = array_keys($emails);
+
+        if (!in_array($email, $emails)) {
+            throw new \Exception(
+                sprintf(
+                    'The message with the subject "%s" has not been sent to "%s", found recipients are "%s"',
+                    $subject,
+                    $email,
+                    implode(
+                        '" and "',
+                        $emails
+                    )
+                )
+            );
+        }
     }
 
     protected function getWatchableTokens()
