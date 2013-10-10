cbe9a1ae9c5820f0c6dce225155a317d9c2368c7
pedro <plazanet.pierre@gmail.com>
2013-10-10 19:15:24 +0200

    Refactor EntityResolver

--- src/Knp/FriendlyContexts/Doctrine/EntityResolver.php
+++ src/Knp/FriendlyContexts/Doctrine/EntityResolver.php
@@ -12,17 +12,17 @@ class EntityResolver
 
     public function resolve(ObjectManager $entityManager, $name, $namespaces)
     {
-        $namespaces = is_array($namespaces) ? $namespaces : [ $namespaces ];
-
-        $allMetadata = $entityManager->getMetadataFactory()->getAllMetadata();
-        $allClass = $this->getObjectReflector()->getReflectionsFromMetadata($allMetadata);
-
-        $names = $this->entityNameProposal($name);
-
         $results = [];
+        if (is_array($namespaces)) {
+            foreach ($namespaces as $namespace) {
+                $results = array_merge($results, $this->resolve($entityManager, $name, $namespace) ?: []);
+            }
+        } else {
+            $namespace = $namespaces;
+            $allMetadata = $entityManager->getMetadataFactory()->getAllMetadata();
+            $allClass = $this->getObjectReflector()->getReflectionsFromMetadata($allMetadata);
 
-        foreach ($namespaces as $namespace) {
-            foreach ($names as $name) {
+            foreach ($this->entityNameProposal($name) as $name) {
                 $class = array_filter(
                     $allClass,
                     function ($e) use ($namespace, $name) {
@@ -30,19 +30,14 @@ class EntityResolver
                         return '' === $namespace
                             ? $nameValid
                             : $namespace === substr($e->getNamespaceName(), 0, strlen($namespace)) && $nameValid
-                        ;
+                            ;
                     }
                 );
-
                 $results = array_merge($results, $class);
-            }
-            if (0 < count($results)) {
-
-                return $results;
             }
         }
 
-        return $results;
+        return (0 < count($results)) ? $results : null;
     }
 
     public function getMetadataFromProperty(ObjectManager $entityManager, $entity, $property)
