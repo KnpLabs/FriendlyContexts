fd2ad605e11c00120309e6c2fb1e8b8e468e52fc
pedro <plazanet.pierre@gmail.com>
2013-10-17 14:30:32 +0200

    Add config tree

--- src/Knp/FriendlyContexts/Extension.php
+++ src/Knp/FriendlyContexts/Extension.php
@@ -3,6 +3,9 @@
 namespace Knp\FriendlyContexts;
 
 use Behat\Behat\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Behat\Gherkin\Loader\YamlFileLoader;
 
 class Extension implements ExtensionInterface
 {
@@ -10,5 +13,35 @@ class Extension implements ExtensionInterface
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__));
         $loader->load('core.yml');
+    }
+
+    public function getConfig(ArrayNodeDefinition $builder)
+    {
+        $builder
+            ->children()
+                ->arrayNode('Contexts')
+                    ->isRequired()
+                        ->children()
+                            ->arrayNode('Entity')
+                                ->children()
+                                    ->scalarNode('enable')->defaultFalse()->end()
+                                    ->arrayNode('namespaces')->end()
+                                ->end()
+                            ->end()
+                            ->arrayNode('SwiftMailer')
+                                ->children()
+                                    ->scalarNode('enable')->defaultFalse()->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+
+    public function getCompilerPasses()
+    {
+        return [];
     }
 }
