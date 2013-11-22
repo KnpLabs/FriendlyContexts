bca88b5ccd540b50e1f5cd11818ef7bc39126871
pedro <plazanet.pierre@gmail.com>
2013-11-22 18:37:48 +0100

    Add int spec content

--- spec/Knp/FriendlyContexts/Guesser/IntGuesserSpec.php
+++ spec/Knp/FriendlyContexts/Guesser/IntGuesserSpec.php
@@ -11,4 +11,43 @@ class IntGuesserSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Knp\FriendlyContexts\Guesser\IntGuesser');
     }
+
+    function it_should_support_smallint_mapping()
+    {
+        $mapping = [
+            'fieldName'  => "number",
+            'type'       => "int",
+            'scale'      => 0,
+            'length'     => null,
+            'unique'     => false,
+            'nullable'   => false,
+            'precision'  => 0,
+            'columnName' => "number",
+        ];
+
+        $this->supports($mapping)->shouldReturn(true);
+    }
+
+    function it_should_support_other_mapping()
+    {
+        $mapping = [
+            'fieldName'  => "created_at",
+            'type'       => "datetime",
+            'scale'      => 0,
+            'length'     => null,
+            'unique'     => false,
+            'nullable'   => false,
+            'precision'  => 0,
+            'columnName' => "created_at",
+        ];
+
+        $this->supports($mapping)->shouldReturn(false);
+    }
+
+    function it_should_transform_string_to_int()
+    {
+        $this->transform("2")->shouldReturn(2);
+        $this->transform("2.6")->shouldReturn(3);
+        $this->transform(" ")->shouldReturn(0);
+    }
 }
