854e0fd3f757ae2c1899376bcaf9b23d0295dde6
pedro <plazanet.pierre@gmail.com>
2013-12-07 18:03:12 +0100

    Fix specs

--- spec/Knp/FriendlyContexts/Tester/ScenarioTesterSpec.php
+++ spec/Knp/FriendlyContexts/Tester/ScenarioTesterSpec.php
@@ -7,6 +7,18 @@ use Prophecy\Argument;
 
 class ScenarioTesterSpec extends ObjectBehavior
 {
+    /**
+     * @param Behat\Behat\Context\ContextDispatcher $dispatcher
+     * @param Knp\FriendlyContexts\Tester\StepTester $tester
+     * @param Symfony\Component\DependencyInjection\ContainerInterface $container
+     **/
+    function let($dispatcher, $tester, $container)
+    {
+        $container->get('behat.context.dispatcher')->willReturn($dispatcher);
+
+        $this->beConstructedWith($container, $dispatcher, $tester);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Knp\FriendlyContexts\Tester\ScenarioTester');
