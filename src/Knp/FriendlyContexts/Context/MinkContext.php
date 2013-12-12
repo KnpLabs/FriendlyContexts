e27689d21cb129f093a8e0373f4b18703c17a0dd
Brucewouaigne <charrier.denis@gmail.com>
2013-12-12 14:39:20 +0100

    Fix mink context behavior

---
+++ src/Knp/FriendlyContexts/Context/MinkContext.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Knp\FriendlyContexts\Context;
+
+use Behat\MinkExtension\Context\MinkContext as BaseMinkContext;
+use Behat\Mink\Exception\ElementNotFoundException;
+
+class MinkContext extends BaseMinkContext
+{
+    public function clickLink($link)
+    {
+        $page  = $this->getSession()->getPage();
+        $links = $page->findAll('named', ['link', $link]);
+
+        foreach ($links as $element) {
+            if ($link=== $element->getText()) {
+                $element->click();
+                return;
+            }
+        }
+
+        throw new ElementNotFoundException(sprintf(
+            'Link "%s" not found', $link
+        ));
+    }
+
+    /**
+     * @When /^(?:|I )follow the link containing "(?P<link>(?:[^"]|\\")*)"$/
+     */
+    public function clickLinkContaining($link)
+    {
+        parent::clickLink($link);
+    }
+}
