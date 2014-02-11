91483c4ed069903a8f341219b1345ef4b616020a
pedro <plazanet.pierre@gmail.com>
2014-02-11 14:36:02 +0100

    Add check/radio steps

--- src/Knp/FriendlyContexts/Context/MinkContext.php
+++ src/Knp/FriendlyContexts/Context/MinkContext.php
@@ -12,26 +12,23 @@ class MinkContext extends BaseMinkContext
      **/
     public function clickElement($name, $element, $nbr = 1, $filterCallback = null)
     {
-        $page  = $this->getSession()->getPage();
-        $elements = $page->findAll('named', array(
-            $element, $this->getSession()->getSelectorsHandler()->xpathLiteral($name)
-        ));
+        $this->elementAction($name, $element, $nbr, function ($e) { $e->click(); }, $filterCallback);
+    }
 
-        if (null !== $filterCallback && is_callable($filterCallback)) {
-            $elements = array_filter($elements, $filterCallback);
-            $elements = array_values($elements);
-        }
-
-        $nbr = -1 === $nbr ? count($elements) : $nbr;
-
-        if ($nbr > count($elements)) {
-            throw new \Exception(sprintf(
-                'Expected to find almost %s "%s" %s, %s found', $nbr, $name, $element, count($elements)
-            ));
-        }
-
-        $e = $elements[$nbr - 1];
-        $e->click();
+    /**
+     * @When /^(?:|I )(?P<state>check|uncheck) the "(?P<name>[^"]*)" (?P<element>radio|checkbox)$/
+     * @When /^(?:|I )(?P<state>check|uncheck) the first "(?P<name>[^"]*)" (?P<element>radio|checkbox)$/
+     * @When /^(?:|I )(?P<state>check|uncheck) the (?P<nbr>\d*)(st|nd|rd|th) "(?P<name>[^"]*)" (?P<element>radio|checkbox)$/
+     **/
+    public function checkElement($state, $name, $element, $nbr = 1)
+    {
+        $this->elementAction(
+            $name,
+            'field',
+            $nbr,
+            function ($e) use ($state)   { if ('check' === $state) { $e->check(); } else { $e->uncheck(); } },
+            function ($e) use ($element) { return $element === $e->getAttribute('type'); }
+        );
     }
 
     /**
@@ -53,5 +50,30 @@ class MinkContext extends BaseMinkContext
     public function clickLink($link)
     {
         $this->clickElement($link, 'link', 1, function ($e) use ($link) { return $link === $e->getText(); });
+    }
+
+    protected function elementAction($locator, $element, $nbr = 1, $actionCallback, $filterCallback = null)
+    {
+        $page  = $this->getSession()->getPage();
+        $elements = $page->findAll('named', array(
+            $element, $this->getSession()->getSelectorsHandler()->xpathLiteral($locator)
+        ));
+
+        if (null !== $filterCallback && is_callable($filterCallback)) {
+            $elements = array_filter($elements, $filterCallback);
+            $elements = array_values($elements);
+        }
+
+        $nbr = -1 === $nbr ? count($elements) : $nbr;
+
+        if ($nbr > count($elements)) {
+            throw new \Exception(sprintf(
+                'Expected to find almost %s "%s" %s, %s found', $nbr, $locator, $element, count($elements)
+            ));
+        }
+
+        $e = $elements[$nbr - 1];
+
+        $actionCallback($e);
     }
 }
