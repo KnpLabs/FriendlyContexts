aba9e3e676b51985537e9b8720d8fe6d58a1fbea
umpirsky <umpirsky@gmail.com>
2014-10-25 18:03:35 +0200

    Profiler and mailer helpers

---
+++ src/Knp/FriendlyExtension/Context/Helper/SwiftMailerHelper.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Knp\FriendlyExtension\Context\Helper;
+
+use Knp\FriendlyExtension\Context\Helper\AbstractHelper;
+
+class SwiftMailerHelper extends AbstractHelper
+{
+    public function countEmailsSent()
+    {
+        return $this->getCollector()->getMessageCount();
+    }
+
+    public function isEmailSent($subject = null, $to = null)
+    {
+        foreach ($this->getCollector()->getMessages('default') as $message) {
+            if ((null === $subject || $subject === trim($message->getSubject())) &&
+                (null === $to || array_key_exists($to, $message->getTo()))
+            ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function getCollector()
+    {
+        return $this->get('swiftmailer')->getCollector('swiftmailer');
+    }
+}
